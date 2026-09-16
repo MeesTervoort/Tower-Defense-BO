89b3114bcacbab1fb265ee9ad107645e3a5d9550
MeesTervoort <39862@ma-web.nl>
2026-09-16 15:50:48 +0200

    towaplacer

--- Assets/TestTD/scripts/TowerPlacer.cs
+++ Assets/TestTD/scripts/TowerPlacer.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 public class TowerPlacer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Tilemap PlacementMap;
+    public Tilemap NonPlaceableMap;
 
-    // Update is called once per frame
+    public GameObject GhostPrefab;
+
+    private HashSet<Vector3Int> OccupiedTiles = new HashSet<Vector3Int>();
+    private GameObject GhostInstance;
+
     void Update()
     {
-        
+       HandlePlacementHover();
+       HandlePlacementClick();
+
+
+    }
+    void HandlePlacementHover()
+    {
+        if(TowerSelectionUI.SelectedTowerPrefab == null)
+        {
+            if(GhostPrefab != null)
+            {
+                Destroy(GhostPrefab);
+            }
+            return;
+
+            if (GhostPrefab == null)
+                GhostInstance = Instantiate(GhostPrefab);
+
+            GhostInstance.GetComponent<SpriteRenderer>().sprite = TowerSelectionUI.SelectedTowerPrefab.GetComponent<SpriteRenderer>().sprite;
+
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0;
+        }
+    }
+    void HandlePlacementClick()
+    {
+
     }
 }
